8b780bb3c593316ad6704c08893f40e17a25b942
barriossteven <barriossteven@gmail.com>
2016-05-31 21:22:24 -0400

    added main and note activities

--- app/src/main/java/com/example/barrios/notepad/MainActivity.java
+++ app/src/main/java/com/example/barrios/notepad/MainActivity.java
@@ -1,13 +1,126 @@
 package com.example.barrios.notepad;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView LVnotes;
+    ArrayList<String> ListOfNotes;
+    private Button newNote;
+    public static final String MyPREFERENCES = "myPref";
+    public static final String JsonKey = "jsonData";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LVnotes = (ListView)findViewById(R.id.ListOfNotes);
+        ListOfNotes = new ArrayList<String>();
+        newNote = (Button)findViewById(R.id.addbutton);
+
+        try {
+            getNotes();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        LVnotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                displayNote(position);
+            }
+        });
+
+        newNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               newNote();
+            }
+        });
+
+    }
+
+    public void getNotes() throws JSONException {
+        ListOfNotes.clear();
+        SharedPreferences sp = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String strJson = sp.getString(JsonKey,null);
+        if(strJson == null){
+            JSONArray jsonArray = new JSONArray();
+            JSONObject notesObj = new JSONObject();
+            try {
+                notesObj.put("Notes",jsonArray);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString(JsonKey, notesObj.toString());
+            editor.commit();
+
+        }else{
+            JSONObject jsonData = new JSONObject(strJson);
+            JSONArray jArray = jsonData.getJSONArray("Notes");
+            for(int i = 0; i<jArray.length(); i++){
+                JSONObject json_data = jArray.getJSONObject(i);
+                ListOfNotes.add(json_data.getString("Note_Name"));
+                populateListView();
+            }
+        }
+
+    }
+
+    public void populateListView(){
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,ListOfNotes);
+
+        LVnotes.setAdapter(arrayAdapter);
+    }
+
+    public void displayNote(int pos){
+        Bundle bundle = new Bundle();
+        bundle.putString("noteName", ListOfNotes.get(pos).toString());
+        Intent intent = new Intent(this,NoteActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void newNote(){
+        Bundle bundle = new Bundle();
+        bundle.putString("noteName","");
+        Intent intent = new Intent(this,NoteActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onRestart() {// TODO Auto-generated method stub
+        super.onRestart();
+
+        //Do your code here
+        try {
+            getNotes();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/barrios/notepad/NoteActivity.java
+++ app/src/main/java/com/example/barrios/notepad/NoteActivity.java
@@ -1,17 +1,48 @@
 package com.example.barrios.notepad;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NoteActivity extends AppCompatActivity {
 
     private Button saveButton;
-    private TextView noteName;
+    private EditText noteName;
     private EditText noteText;
+    private String note;
+    private String prevNoteName;
+    private SharedPreferences sharedPreferences;
+    private String strJson;
+
+    //TextWatcher
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3)
+        {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+            checkFieldsForEmptyValues();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +50,155 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
 
         saveButton = (Button) findViewById(R.id.saveButton);
-        noteName = (TextView) findViewById(R.id.noteName);
+        noteName = (EditText) findViewById(R.id.noteName);
+        noteName.addTextChangedListener(textWatcher);
         noteText = (EditText) findViewById(R.id.noteText);
+        sharedPreferences = loadPreferences();
+        checkFieldsForEmptyValues();
+
+        Bundle bundle = getIntent().getExtras();
+        prevNoteName = bundle.getString("noteName");
+
+
+        if(!prevNoteName.equals("")){
+            noteName.setText(prevNoteName);
+            try {
+                loadNote(prevNoteName);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                noteName.setText("save was clicked");
-                //yess
+                try {
+                    savePreferences();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+
+
+
+
+    }
+    private void loadNote(String noteName) throws JSONException {
+        JSONObject noteobj = new JSONObject(strJson);
+        JSONArray notes = noteobj.getJSONArray("Notes");
+
+        for(int i = 0; i< notes.length(); i++){
+            JSONObject note = (JSONObject)notes.get(i);
+            if(note.getString("Note_Name").equals(prevNoteName)){
+                noteText.setText(note.getString("Note_Text"));
+            }
+        }
+
+
+
+    }
+    private SharedPreferences loadPreferences(){
+
+        SharedPreferences sp = getSharedPreferences(MainActivity.MyPREFERENCES, Context.MODE_PRIVATE);
+        strJson = sp.getString(MainActivity.JsonKey,null);
+        return sp;
+    }
+    private void savePreferences() throws JSONException {
+        String nName = noteName.getText().toString();
+
+        if(prevNoteName.equals("")){
+           addNewNote();
+        }else if(nName.equals(prevNoteName)){
+            //dont update name just note
+            updateNote();
+        }else {
+            //update name and note
+            updateNandT();
+        }
+
+
+    }
+
+    private void updateNandT() throws JSONException {
+        String nText = noteText.getText().toString();
+        String nName = noteName.getText().toString();
+
+        JSONObject noteobj = new JSONObject(strJson);
+        JSONArray notes = noteobj.getJSONArray("Notes");
+        for(int i = 0; i< notes.length(); i++){
+            JSONObject note = (JSONObject)notes.get(i);
+            if(note.getString("Note_Name").equals(prevNoteName)){
+                ((JSONObject)notes.get(i)).put("Note_Text", nText);
+                ((JSONObject)notes.get(i)).put("Note_Name", nName);
+                prevNoteName = nName;
+            }
+        }
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MainActivity.JsonKey, noteobj.toString());
+        editor.commit();
+        Toast.makeText(NoteActivity.this,"Note Saved",Toast.LENGTH_LONG).show();
+
+    }
+    private void addNewNote() throws JSONException {
+        String nText = noteText.getText().toString();
+        String nName = noteName.getText().toString();
+
+        JSONObject noteobj = new JSONObject(strJson);
+        JSONArray notes = noteobj.getJSONArray("Notes");
+        JSONObject note = new JSONObject();
+        note.put("Note_Name", nName);
+        note.put("Note_Text", nText);
+        notes.put(note);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MainActivity.JsonKey, noteobj.toString());
+        editor.commit();
+        Toast.makeText(NoteActivity.this,"Note Saved",Toast.LENGTH_LONG).show();
+    }
+
+    private void updateNote() throws JSONException {
+        String nText = noteText.getText().toString();
+
+        JSONObject noteobj = new JSONObject(strJson);
+        JSONArray notes = noteobj.getJSONArray("Notes");
+        for(int i = 0; i< notes.length(); i++){
+            JSONObject note = (JSONObject)notes.get(i);
+            if(note.getString("Note_Name").equals(prevNoteName)){
+                ((JSONObject)notes.get(i)).put("Note_Text", nText);
+            }
+        }
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MainActivity.JsonKey, noteobj.toString());
+        editor.commit();
+        Toast.makeText(NoteActivity.this,"Note Saved",Toast.LENGTH_LONG).show();
+    }
+
+    private  void checkFieldsForEmptyValues(){
+
+        String s1 = noteName.getText().toString().trim();
+
+        if(s1.trim().isEmpty())
+        {
+            saveButton.setEnabled(false);
+        }else
+        {
+            saveButton.setEnabled(true);
+        }
 
     }
 
 
 
-
-
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        super.onBackPressed();
+        finish();
+    }
 
 
 
